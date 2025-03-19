b9180e2d77ad581795e6c785ffe39ebadca0cc7a
umer <umeraftab763@gmail.com>
2025-03-19 16:56:59 +0500

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          PERN Stack 
+          PERN Stack ahahah
         </a>
       </header>
     </div>
