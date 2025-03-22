2ccccaef4502d547210394ecd8db366d64887797
umer <umeraftab763@gmail.com>
2025-03-22 05:15:23 +0500

    save

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/data', {
+      const response = await axios.post('https://pern-liard.vercel.app/api/data', {
         name,
         email,
       });
